f2f3d7ed02c7f9a58c0b2c0797eb22251bf2dac0
changliu8 <lc928831980@hotmail.com>
2025-06-04 22:11:34 +0000

    hero also loses

--- oop/main.cpp
+++ oop/main.cpp
@@ -21,11 +21,17 @@ int main(){
             h1->attack((*pirates)[i]);
             (*pirates)[i]->display();
             if((*pirates)[i]->getHealth()<=0){
+                std::cout << "Pirate " << i+1 << " is done!" << std::endl;
                 break;
             }
             (*pirates)[i]->attack(h1);
+            if(h1->getHealth()<=0){
+                h1->display();
+                std::cout << "Hero is DONE!!!!!!!!!" << std::endl;
+                std::cout << "\033[31mRemaining pirate: " << size-(i+1) << "\033[0m" << std::endl;
+                return 0;
+            }
             h1->display();
         }
-        std::cout << "Pirate " << i+1 << " is done!" << std::endl;
     }
 }
